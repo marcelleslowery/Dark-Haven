6e5cee450ea5e1040ea0996e2f149a48fb172332
Marcelles L'von Lowery <marcelleslowery@gmail.com>
2017-04-11 14:34:57 -0400

    First Commit
    Adding Level 1

---
+++ Assets/Scripts/LRFloaters.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LRFloaters : MonoBehaviour {
+
+	Transform floater;
+	Vector3 myPos;
+	bool stopped;
+
+	void Start () {
+		
+		floater = this.transform;
+		myPos = new Vector3 (0.1f, 0, 0);
+		stopped = false;
+	}
+
+	void Update () {
+
+		if (floater.position.x >= 11f) {
+			myPos.x = -0.1f;
+		}
+		if (floater.position.x <= -11f) {
+			myPos.x = 0.1f;
+		}
+
+		if (stopped) {
+			//nuffin
+		} else {
+			floater.position += myPos;
+		}
+	}
+}
