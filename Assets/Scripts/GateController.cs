75a7a321bb8097b1c606fa862daf44235218c8f9
Marcelles L'von Lowery <marcelleslowery@gmail.com>
2017-04-11 15:58:23 -0400

    Lvl 2 Started
    Adding Lvl 2

---
+++ Assets/Scripts/GateController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GateController : MonoBehaviour {
+
+	Transform gate;
+	Vector3 myPos;
+	bool stopped;
+
+	void Start () {
+
+		gate = this.transform;
+		myPos = new Vector3 (0, 0.1f, 0);
+		stopped = false;
+	}
+
+	void Update () {
+
+		if (gate.position.y >= 11f) {
+			myPos.y = -0.5f;
+		}
+		if (gate.position.y <= 4f) {
+			myPos.y = 0.2f;
+		}
+
+		if (stopped) {
+			//nuffin
+		} else {
+			gate.position += myPos;
+		}
+	}
+}
