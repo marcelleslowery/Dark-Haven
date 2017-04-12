d7cce3158fbabdbc7224927473b63d9a333cb946
John Blum <jblum@gatech.edu>
2017-04-12 11:02:36 -0400

    scenes move forward now

---
+++ Assets/nextScene.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class nextScene : MonoBehaviour {
+    private void OnTriggerEnter(Collider other)
+    {
+        int nextScene = SceneManager.GetActiveScene().buildIndex + 1;
+        SceneManager.LoadScene(nextScene);
+    }
+}
