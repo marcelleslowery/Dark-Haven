9c29ef0907d48012b80604f60716ffa4e22f66d6
John Blum <jblum@gatech.edu>
2017-04-12 08:57:41 -0400

    added pendulum to main room

---
+++ Assets/pendulumScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pendulumScript : MonoBehaviour {
+    public float angle = 15.0f;
+    public Vector3 rotationAxis = new Vector3(0, 0, 1);
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
