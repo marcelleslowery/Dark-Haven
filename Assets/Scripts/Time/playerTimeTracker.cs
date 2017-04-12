f7bc2d778b8b53441747f5a3a23ccf3f0b20cc00
Blum, John W <jblum@gatech.edu>
2017-04-12 00:41:50 -0400

    timestuffpt2

---
+++ Assets/Scripts/Time/playerTimeTracker.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(LazyMovement))]
+public class playerTimeTracker : timeTracker
+{
+}
