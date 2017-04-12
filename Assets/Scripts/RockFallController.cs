f2648e4f895ccd0bd3ee5507e9fe0e92f394f6b3
Alex Poole <Alex Poole>
2017-04-12 07:18:32 -0400

    Trying to merge things...

---
+++ Assets/Scripts/RockFallController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockFallController : MonoBehaviour {
+
+    private Vector3 lastpos;
+    private bool falling = false;
+	// Use this for initialization
+	void Start () {
+        lastpos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 currentPos = transform.position;
+        if (!falling && (lastpos.y > currentPos.y))
+        {
+            falling = true;
+            gameObject.GetComponent<AudioSource>().Play();
+        }
+        lastpos = currentPos;
+	}
+
+    void OnTriggerEnter(Collider col)
+    {
+
+        if (col.tag == "EndTrigger")
+        {
+            gameObject.GetComponent<Rigidbody>().isKinematic = true;
+            gameObject.GetComponent<Rigidbody>().useGravity = false;
+            gameObject.tag = "EndTrigger";
+        }
+    }
+}
