131367edd5077300dbc6d6206c565159fe7ba305
Alex Poole <Alex Poole>
2017-04-11 23:01:52 -0400

    Adding player

---
+++ Assets/Scripts/CamController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamController : MonoBehaviour {
+
+	Transform myCam;
+	Vector3 camPos;
+
+	// Use this for initialization
+	void Start () {
+		myCam = this.transform;
+		camPos = new Vector3 (0f, 0f, 0.1f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		myCam.position += camPos;
+	}
+}
---
+++ Assets/Scripts/UDFloaters.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UDFloaters : MonoBehaviour {
+
+	Transform floater;
+	Vector3 myPos;
+	bool stopped;
+
+	void Start () {
+
+		floater = this.transform;
+		myPos = new Vector3 (0, 0.1f, 0);
+		stopped = false;
+	}
+
+	void LateUpdate () {
+
+		if (floater.position.y >= 18f) {
+			myPos.y = -0.1f;
+		}
+		if (floater.position.y <= 7f) {
+			myPos.y = 0.1f;
+		}
+
+		if (stopped) {
+			//nuffin
+		} else {
+			floater.position += myPos;
+		}
+	}
+}
