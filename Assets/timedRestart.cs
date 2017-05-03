f651435c1325b746a00b7ce98a3599578349051c
John Blum <jblum@gatech.edu>
2017-05-03 07:21:23 -0400

    adding end

---
+++ Assets/timedRestart.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class timedRestart : MonoBehaviour {
+    public float timeUntilRestart = 20.0f;
+	// Use this for initialization
+	void Start () {
+        Invoke("goToMain", timeUntilRestart);
+	}
+
+    public void goToMain()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
