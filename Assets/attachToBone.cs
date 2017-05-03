c448299c76a9cbf78a935528d311bed5c8751e7e
John Blum <jblum@gatech.edu>
2017-05-03 05:30:33 -0400

    ignore the decal on player back

---
+++ Assets/attachToBone.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class attachToBone : MonoBehaviour {
+
+    public HumanBodyBones selectedBone = HumanBodyBones.Spine;
+	void Start () {
+        this.transform.parent = this.GetComponentInParent<Animator>().GetBoneTransform(selectedBone);
+    }
+}
