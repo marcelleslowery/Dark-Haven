99897320691568af0018c0f5d17453a88d38f301
Alex Poole <Alex Poole>
2017-05-03 06:53:50 -0400

    Adding death

---
+++ Assets/RagdollEnabled.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RagdollEnabled : MonoBehaviour {
+
+    Rigidbody[] bodyComponents;
+	// Use this for initialization
+	void Start () {
+        bodyComponents = gameObject.GetComponentsInChildren<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	public void Kill () {
+		foreach (Rigidbody comp in bodyComponents)
+        {
+            comp.isKinematic = false;
+        }
+        Animator animator = gameObject.GetComponent<Animator>();
+        animator.enabled = false;
+        Invoke("Reset", 5f);
+	}
+
+    public void Reset ()
+    {
+        SceneManager.LoadScene("BetterCharacter");
+    }
+}
---
+++ Assets/Unragdoll.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unragdoll : MonoBehaviour {
+
+    private Rigidbody playerRigidbody;
+    private Animator playerAnimator;
+
+
+    void Start()
+    {
+        playerRigidbody = GetComponent<Rigidbody>();
+        playerAnimator = GetComponent<Animator>();
+        SetKinematic(true);
+    }
+
+
+    public void SetKinematic(bool newValue)
+    {
+
+        //Get an array of components that are of type Rigidbody
+        Component[] components = GetComponentsInChildren(typeof(Rigidbody));
+
+        //For each of the components in the array, treat the component as a Rigidbody and set its isKinematic and detectCollisions property
+        foreach (Component c in components)
+        {
+            (c as Rigidbody).isKinematic = newValue;
+            (c as Rigidbody).detectCollisions = !newValue;
+        }
+
+        //Sets PLAYER rigid body as opposite
+        playerRigidbody.isKinematic = !newValue;
+        playerRigidbody.detectCollisions = newValue;
+        playerAnimator.enabled = newValue;
+
+    }
+}
