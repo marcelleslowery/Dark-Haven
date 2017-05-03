30f97d272f9326a63ac83da4706d73a03d290af1
Marcelles L'von Lowery <marcelleslowery@gmail.com>
2017-05-03 09:42:12 -0400

    Adding sound
    Lvl music and most foley sounds added

---
+++ Assets/Scripts/Footsteps.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Adapted from footsteps tutorial: https://www.youtube.com/watch?v=ISoBKFxQLic
+
+[RequireComponent (typeof (AudioSource))]
+
+public class Footsteps : MonoBehaviour {
+
+	AudioSource myAudio;
+	Animator myAnim;
+	public AudioClip floor;
+	public GameObject leftFoot;
+	public GameObject rightFoot;
+
+	[Space(5.0f)]
+	private float currentVolume;
+	[Range(0.0f,1.0f)]
+	public float volume = 0.15f;
+	[Range(0.0f,0.2f)]
+	public float volumeVariance = 0.095f;
+	private float pitch;
+	[Range(0.0f,0.2f)]
+	public float pitchVariance = 0.08f;
+	[Space(5.0f)]
+
+	float currentFootstepLeft;
+	float lastFootstepLeft;
+	float currentFootstepRight;
+	float lastFootstepRight;
+
+	void Start () {
+
+		myAudio = gameObject.GetComponent<AudioSource>();
+		myAnim = GetComponent<Animator> ();
+	}
+
+	void OnCollisionStay(Collision obj) {
+		//Left foot
+		currentFootstepLeft = myAnim.GetFloat ("FootstepLeft");
+		if (currentFootstepLeft > 0f && lastFootstepLeft < 0f) {
+
+			CheckGround (obj.gameObject, leftFoot);
+		}
+		lastFootstepLeft = myAnim.GetFloat ("FootstepLeft");
+
+		//Right foot
+		currentFootstepRight = myAnim.GetFloat ("FootstepRight");
+		if (currentFootstepRight < 0f && lastFootstepRight > 0f) {
+
+			CheckGround (obj.gameObject, rightFoot);
+		}
+		lastFootstepRight = myAnim.GetFloat ("FootstepRight");
+	}
+
+	void CheckGround(GameObject floor, GameObject currentFoot){
+
+		if (floor.tag == ("Floor")) {
+			Invoke ("PlayConcrete", 0);
+		}
+	}
+
+	void PlayConcrete(){
+		currentVolume = (volume + UnityEngine.Random.Range (-volumeVariance, volumeVariance));
+		pitch = (1.0f + Random.Range (-pitchVariance, pitchVariance));
+		myAudio.pitch = pitch;
+		myAudio.PlayOneShot (floor, currentVolume);
+	}
+}
--- Assets/Scripts/GateController.cs
+++ Assets/Scripts/GateController.cs
@@ -3,11 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(timeTracker))]
+[RequireComponent(typeof(AudioSource))]
 public class GateController : MonoBehaviour {
 
 	Transform gate;
 	Vector3 myPos;
 	bool stopped;
+
+	AudioSource myAudio;
+	public AudioClip hit;
+	[Space(5.0f)]
+	private float currentVolume;
+	[Range(0.0f,1.0f)]
+	public float volume = 0.15f;
+	[Range(0.0f,0.2f)]
+	public float volumeVariance = 0.095f;
+	private float pitch;
+	[Range(0.0f,0.2f)]
+	public float pitchVariance = 0.08f;
+	[Space(5.0f)]
 
     timeTracker tT;
 
@@ -16,6 +30,7 @@ public class GateController : MonoBehaviour {
 		gate = this.transform;
 		myPos = new Vector3 (0, 0.1f, 0);
 		stopped = false;
+		myAudio = gameObject.GetComponent<AudioSource>();
         tT = GetComponent<timeTracker>();
     }
 
@@ -28,6 +43,7 @@ public class GateController : MonoBehaviour {
 			myPos.y = -0.5f;
 		}
 		if (gate.position.y <= 4f) {
+			Invoke ("PlaySmash", 0);
 			myPos.y = 0.2f;
 		}
 
@@ -36,5 +52,12 @@ public class GateController : MonoBehaviour {
 		} else {
 			gate.position += myPos;
 		}
+	}
+
+	void PlaySmash(){
+		currentVolume = (volume + UnityEngine.Random.Range (-volumeVariance, volumeVariance));
+		pitch = (1.0f + Random.Range (-pitchVariance, pitchVariance));
+		myAudio.pitch = pitch;
+		myAudio.PlayOneShot (hit, currentVolume);
 	}
 }
